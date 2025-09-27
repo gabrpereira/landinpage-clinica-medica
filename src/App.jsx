ccd94c4418c985bcaccf53f55eabd94359412821
Gabriel Pereira DEV <arthur738pereira@gmail.com>
2025-09-27 11:06:21 -0300

    Estruturação do app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import Menu_page_01 from "./components/menu/Menu_page_01";
+import Header from "./components/header/Header";
 import Card_01 from "./components/section/Card_01";
 import Card_02 from "./components/section/Card_02";
+import Btn_lean_more from "./components/button/Btn_lean_more";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div>
+      <Header />
       <Menu_page_01 />
       <Card_01 />
       <Card_02 />
+      <Btn_lean_more />
+      <Footer />
     </div>
   );
 }
