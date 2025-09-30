cd14d080753753432fc8cd7a33409e1e45efe8c6
OKleisonSilva <o.eriksilva@outlook.com>
2025-09-30 00:10:53 -0300

    Código do Header completo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,20 @@
+
+
 import "./Header.css";
 
-function Header () {
+function Header() {
     return (
-        null
+        <header className="header">
+            <div className="h1">
+            <h1 className="logo">Clínica Médica</h1>
+            </div>
+            <nav className="nav">
+                <a className="nav-button"> Início</a>
+                <a className="nav-button">Serviços</a>
+                <a className="nav-button">Contato</a>
+                <a className="nav-button">Sobre</a>
+            </nav>
+        </header>
     );
 }
 
